a53e1a89856b4bd10eccc85dd2147250557f3b3a
18547995 <18547995@sigma.sbrf.ru>
2020-11-23 20:39:55 +0300

    [FEATURE] thread learn

---
+++ src/RunnableThread.java
@@ -0,0 +1,22 @@
+public  class RunnableThread {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> RunnableThread.sillyWork("run"));
+        t1.start();
+
+        sillyWork("main");
+    }
+
+    private static void sillyWork(String word){
+        double d = 0;
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 100_000; j++) {
+                d = Math.sin(d);
+            }
+
+            System.out.println(word);
+        }
+
+    }
+}
